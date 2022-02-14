911532ec23ba2a0972324a0980782e62f6a5db97
ht2390 <ht2390@columbia.edu>
2022-02-14 12:47:36 -0500

    Update testproject.py

--- example_code/testproject.py
+++ example_code/testproject.py
@@ -3,11 +3,8 @@ import experiment1 as ex1
 import experiment2 as ex2
 
 def author():
-        return 'htang63'
+        return 'Your name here'
 
 if __name__ == "__main__":
-#    man.portval_in_sample_plot()
-#    man.portval_out_sample_plot()
     ex1.e1_in_sample()
     ex1.e1_out_sample()
-#    ex2.e2()
